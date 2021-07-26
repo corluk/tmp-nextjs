371491b1880eadcd8f2e3a5aa34e4d87598d1fe3
corluk <corluk@hotmail.com>
2021-07-26 17:43:45 +0300

    d

---
+++ pages/index.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Link from "next/link"
+import Btn from "../components/Btn"
+import posts from "../data/posts"
+const Component = () => (
+    <div>
+        <div>
+            {posts.map((post,index)=>(
+                 <li key={index}>
+
+                     <Link as={`/post/${index}`} href={{pathname:"/second",query:{id:index}}}>
+                         <a> {post.title} </a>
+                     </Link>
+                 </li>
+            ))}
+            <Link href="/second" passHref>  Children are here </Link>
+        </div>
+        <div> Hello World
+
+        </div>
+        <div>
+            
+        </div>
+    </div>)
+
+export default Component
