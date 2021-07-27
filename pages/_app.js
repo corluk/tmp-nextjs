a05d9713605496dacd84724e31579ad54032d2e1
corluk <corluk@hotmail.com>
2021-07-27 22:18:27 +0300

    d

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import React from "react" 
 import { wrapper } from "../store/store"
-
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 const WrappedApp =({Component,pageProps})=>{
     return <Component {...pageProps}/>
 }
