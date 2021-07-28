1b3f9823edff96ac02f46696297f4d3f5038ed0e
corluk <corluk@hotmail.com>
2021-07-28 10:26:13 +0300

    d

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,23 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 const WrappedApp =({Component,pageProps})=>{
-    return <Component {...pageProps}/>
+
+    React.useEffect(()=>{
+        const jssStyles = document.querySelector("#jss-server-side")
+        if(jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    },[])
+    return <React.Fragment> 
+            <Head>
+                <title> Material enabled</title>
+                <meta name="viewport" content="minimum-scale=1,initial-scale=1,width=device-width"></meta>
+            </Head>
+            <ThemeProvider theme={{palette: {primary:{main:red.A400}}}}>
+                <CssBaseline />
+                <Component {...pageProps}/>
+            </ThemeProvider>
+        </React.Fragment>
 }
 
 export default wrapper.withRedux(WrappedApp)
